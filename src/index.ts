c608dbf2d073100250a2ae574e307ee3dc229491
Max Kostow <max@percolate.com>
2019-02-21 12:45:18 -0800

    echo jwt

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,42 @@
 import * as express from 'express'
+import { verify } from 'jsonwebtoken'
 
 const app = express()
 const port = process.env.PORT || 3000
+const secret = process.env.APP_SECRET
 
 app.use(express.static('public'))
 
-app.get('/campaign', (req, res) => res.send('Hello World!'))
-app.get('/content', (req, res) => res.send('Hello World!'))
-app.get('/asset', (req, res) => res.send('Hello World!'))
-app.get('/request', (req, res) => res.send('Hello World!'))
-app.get('/task', (req, res) => res.send('Hello World!'))
-app.get('/top_nav', (req, res) => res.send('Hello World!'))
-app.get('/settings', (req, res) => res.send('Hello World!'))
+function echoJwt(req: express.Request, res: express.Response) {
+    const token: string = req.query.jwt
+
+    if (!secret) {
+        res.send('No APP_SECRET defined')
+        return
+    }
+
+    try {
+        const decoded = verify(token, secret)
+        res.send(`
+            <h1>jwt</h1>
+            <pre>${token}</pre>
+            <h1>decoded</h1>
+            <pre>${decoded}</pre>
+        `)
+    } catch (e) {
+        res.send(`
+            <h1>invalid jwt</h1>
+            <pre>${token}</pre>
+        `)
+    }
+}
+
+app.get('/campaign', echoJwt)
+app.get('/content', echoJwt)
+app.get('/asset', echoJwt)
+app.get('/request', echoJwt)
+app.get('/task', echoJwt)
+app.get('/top_nav', echoJwt)
+app.get('/settings', echoJwt)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

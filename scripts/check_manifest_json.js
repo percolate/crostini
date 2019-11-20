56e307cb748d5fc94a0514388857571f8a21ed8c
Nicolas Hahn <nicolas@stonespring.org>
2019-11-20 14:56:39 -0500

    replace console.log with throw err

--- scripts/check_manifest_json.js
+++ scripts/check_manifest_json.js
@@ -3,7 +3,7 @@
 var fs = require('fs')
 
 fs.readFile('public/manifest.json', 'utf8', function(err, contents) {
-    console.log(contents)
+    if (err) throw err
     // will cause exit with status 1 if invalid json
     JSON.parse(contents)
 })
